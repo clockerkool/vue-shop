89e8c3ee915088b201e80d110689421e515de922
sultanov_dm <sultanov_dm@taximaxim.ru>
2024-12-09 09:06:06 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import Header from './components/Header.vue';
+</script>
+
+
+<template>
+  <div class="w-4/5 m-auto rounded-xl bg-white shadow-xl mt-14">
+    <Header></Header>
+    <h1>Started</h1>
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header class="flex justify-between border-b border-slate-300 px-8">
+      <div class="flex items-center">
+        <img src="/logo.png" class="m-10 gap-4">
+        <div>
+          <h2 class="text-xl">
+            ClockStyle
+          </h2>
+        </div>
+      </div>
+
+      <ul class="flex items-center gap-8">
+        <li class="flex items-center gap-3 cursor-pointer hover:text-orange-500">
+          <img src="/cart.svg" alt="">
+          <b>Корзина</b>
+        </li>
+
+        <li class="flex items-center gap-3 cursor-pointer hover:text-orange-500">
+          <img src="/heart.svg" alt="">
+          <b>Закладки</b>
+        </li>
+        <li class="flex items-center gap-3 cursor-pointer hover:text-orange-500">
+          <img src="/profile.svg" alt="">
+          <b>Профиль</b>
+        </li>
+      </ul>
+    </header>
+</template>
